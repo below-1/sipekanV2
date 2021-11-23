6787848a8c1c8f109ac8502ad0b7983a30a05794
jordan meta <jordanmeta@mail.com>
2021-11-24 00:02:07 +0800

    add user view pages

--- landing/urls.py
+++ landing/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('info', views.get_info, name='get_info')
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from app import models
+from app.meta_form import meta_form__data_uji_kendaraan
 
 # Create your views here.
 def landing(request):
-    return render(request, 'landing.html')
+    return render(request, 'landing/index.html')
+
+def get_info(request):
+    token = request.GET['token']
+    item = models.DataUjiKendaraan.objects.filter(user_token=token).get()
+    forms = []
+    for group in meta_form__data_uji_kendaraan:
+        components = []
+        group_item = { 
+            'name': group.name,
+            'title': group.title
+        }
+        for comp in group.components:
+            if comp.separator:
+                components.append({
+                    'name': comp.name,
+                    'label': comp.label,
+                    'separator': True
+                })
+            else:
+                components.append({
+                    'name': comp.name,
+                    'label': comp.label,
+                    'value': getattr(item, comp.name)
+                })
+        group_item['components'] = components
+        forms.append(group_item)
+    context = {
+        'item': item,
+        'forms': forms
+    }
+    return render(request, 'landing/info.html', context)
